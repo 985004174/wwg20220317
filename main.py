2eb86b8cc8375eb48873c1ad646438c3052fc59f
kevinD <985004174@qq.com>
2022-06-23 20:32:28 +0800

    2020-06-23-修改4

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main_run():
     pytest.main(['-s','-v', '-W', 'ignore:Module already imported:pytest.PytestWarning',
                  '--alluredir', './report/report', "--clean-alluredir"])
     # pytest.main(['-s','-W', '--alluredir', './report/report', '--clean-alluredir','ignore:Module already imported:pytest.PytestWarning'])
-    os.system('allure generate ./report/report -o ./allure-report --clean')
+    os.system('allure generate ./report/report -o ./report/html --clean')
 
 if __name__ == '__main__':
     main_run()
